8e56d5eac6863a918602fadf179fb8e63e5e66c2
Daming Li <daming.li@yale.edu>
2020-09-17 21:49:41 -0400

    implemented linear model

--- R/linear_model.R
+++ R/linear_model.R
@@ -1,12 +1,30 @@
 #' @title Fits linear model
-#' @description Fits linear model using built-in lm function
-#' @param f the formula
-#' @param df the dataframe
+#' @description Fits linear model by solving normal equation
+#' @param form a formula object
+#' @param dat a dataframe
 #' @param constrasts list of variables as input
 #' @example 
 #' fit <- linear_model(Sepal.Length ~ ., iris, contrasts = list(Species = "contr.sum"))
+#' print(fit)
 #' @export
 
-linear_model <- function(f, df, constrasts=NULL) {
-    lm(f, df, contrasts)
+linear_model <- function(form, dat, contrasts=NULL){
+    if (! is.null(contrasts)){
+      contrasts(dat[,names(contrasts)]) <- contrasts[[names(contrasts)]]
+    }
+  
+    X <- model.matrix(form,dat)
+    y_name <- as.character(form)[2]
+    y <- matrix(model.frame(form,dat)[,y_name], ncol=1)
+    
+    #betas <- solve(t(X)%*%X)%*%t(X)%*%y
+    betas <- solve.qr(qr(X), y)
+    betas[which(betas==0)] = NA
+    betas_name <- rownames(betas)
+    betas <- as.numeric(betas)
+    names(betas) <- betas_name
+    
+    res <- list(coefficients = betas)
+    #lass(res) <- "my_lm"
+    res
 }
